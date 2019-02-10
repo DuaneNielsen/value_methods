a0b3a8b79089e7d371a42ddbdab924fcd710373b
niedu02 <duane.nielsen.rocks@gmail.com>
2019-02-09 20:50:06 -0800

    First commit

---
+++ minimal.py
@@ -0,0 +1,94 @@
+import gym
+import gym_minigrid
+from gym_minigrid.wrappers import *
+import random
+import time
+import cv2
+
+def to_numpyRGB(image, invert_color=False):
+    """
+    Universal method to detect and convert an image to numpy RGB format
+    :params image: the output image
+    :params invert_color: perform RGB -> BGR convert
+    :return: the output image
+    """
+    # remove batch dimension
+    if len(image.shape) == 4:
+        image = image[0]
+    smallest_index = None
+    if len(image.shape) == 3:
+        smallest = min(image.shape[0], image.shape[1], image.shape[2])
+        smallest_index = image.shape.index(smallest)
+    elif len(image.shape) == 2:
+        smallest = 0
+    else:
+        raise Exception(f'too many dimensions, I got {len(image.shape)} dimensions, give me less dimensions')
+    if smallest == 3:
+        if smallest_index == 2:
+            pass
+        elif smallest_index == 0:
+            image = np.transpose(image, [1, 2, 0])
+        elif smallest_index == 1:
+            # unlikely
+            raise Exception(f'Is this a color image?')
+        if invert_color:
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    elif smallest == 1:
+        image = np.squeeze(image)
+    elif smallest == 0:
+        # greyscale
+        pass
+    elif smallest == 4:
+        # I guess its probably the 32-bit RGBA format
+        image = cv2.cvtColor(image, cv2.COLOR_RGBA2BGR)
+    else:
+        raise Exception(f'dont know how to display color of dimension {smallest}')
+    return image
+
+
+class UniImageViewer:
+    def __init__(self, title='title', screen_resolution=(640, 480), format=None, channels=None, invert_color=True):
+        self.C = None
+        self.title = title
+        self.screen_resolution = screen_resolution
+        self.format = format
+        self.channels = channels
+        self.invert_color = invert_color
+
+    def render(self, image, block=False):
+
+        image = to_numpyRGB(image, self.invert_color)
+
+        image = cv2.resize(image, self.screen_resolution)
+
+        # Display the resulting frame
+        cv2.imshow(self.title, image)
+        if block:
+            cv2.waitKey(0)
+        else:
+            cv2.waitKey(1)
+
+    def view_input(self, model, input, output):
+        image = input[0] if isinstance(input, tuple) else input
+        self.render(image)
+
+    def view_output(self, model, input, output):
+        image = output[0] if isinstance(output, tuple) else output
+        self.render(image)
+
+    def update(self, image):
+        self.render(image)
+
+env = FullyObsWrapper(gym.make('MiniGrid-DoorKey-8x8-v0'))
+#env = gym.make('MiniGrid-Empty-6x6-v0')
+v = UniImageViewer()
+
+obs = env.reset()
+done = False
+while not done:
+    action = random.randint(0, env.action_space.n - 1)
+    obs, reward, done, info = env.step(action)
+    v.render(obs)
+    env.render('human')
+    print(obs)
+    time.sleep(1.0)
